7bf713fe6607e5580c4360f0d5a89bfacaa70cd8
Christoph Kindl <christoph.kindl@student.tuwien.ac.at>
2014-10-31 13:14:50 +0100

    Created a skeleton maven project.

---
+++ src/main/java/at/ac/tuwien/infosys/dsg/aic/ws2014/g4/t1/App.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.infosys.dsg.aic.ws2014.g4.t1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
