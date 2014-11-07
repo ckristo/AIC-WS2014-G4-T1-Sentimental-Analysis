2f78ce2176a309b94973972f66fb0c0bb11f00da
Christoph Kindl <christoph.kindl@student.tuwien.ac.at>
2014-11-07 12:46:07 +0100

    Added example on how to load Twitter data.

---
+++ README.md
@@ -0,0 +1,8 @@
+## Startup ##
+
+* Startup of the Twitter example:
+
+```
+mvn compile exec:java -Dexec.mainClass="at.ac.tuwien.infosys.dsg.aic.ws2014.g4.t1.TwitterExample" 
+```
+
---
+++ src/main/java/at/ac/tuwien/infosys/dsg/aic/ws2014/g4/t1/TwitterExample.java
@@ -0,0 +1,93 @@
+package at.ac.tuwien.infosys.dsg.aic.ws2014.g4.t1;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
+
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.Status;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.TwitterObjectFactory;
+
+/**
+ * TODO
+ */
+public class TwitterExample {
+
+	public static void main(String[] args) {
+
+		/**
+		 * EXAMPLE CODE for using the Twitter Search API --
+		 * https://dev.twitter.com/rest/public/search
+		 */
+		// set up twitter instance
+		Twitter twitter = new TwitterFactory().getInstance();
+		Query query = new Query("@twitter OR to:twitter");
+		
+		try {
+			// obtain oauth2 bearer token
+			twitter.getOAuth2Token();
+
+			// execute query and print results
+			QueryResult result = twitter.search(query);
+			for (Status status : result.getTweets()) {
+				System.out.println("@" + status.getUser().getScreenName() + ":"
+						+ status.getText());
+			}
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("----------------------------------------");
+
+		/**
+		 * EXAMPLE CODE for loading a Twitter Stream API dump --
+		 * https://dev.twitter.com/streaming/overview/processing
+		 */
+		InputStream bzIn = null;
+		try {
+			// uses the Apache Commons Compress library
+			// to directly read the bzipped file
+			FileInputStream fin = new FileInputStream("../tweets.txt.bz2");
+			BufferedInputStream in = new BufferedInputStream(fin);
+			bzIn = new BZip2CompressorInputStream(in);
+
+			// set up hbc's StringDelimitedProcessor -- see
+			// https://github.com/twitter/hbc
+			BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
+			StringDelimitedProcessor sdp = new StringDelimitedProcessor(queue);
+			sdp.setup(bzIn);
+
+			// extract some JSON objects from the file
+			int i = 0, limit = 5;
+			while (sdp.process() && i < limit) {
+				Status status = TwitterObjectFactory.createStatus(queue.poll());
+
+				System.out.println("@" + status.getUser().getScreenName() + ":"
+						+ status.getText());
+
+				i++;
+			}
+		} catch (IOException | InterruptedException | TwitterException e) {
+			e.printStackTrace();
+		} finally {
+			// close input stream
+			try {
+				if (bzIn != null)
+					bzIn.close();
+			} catch (IOException e) {
+			}
+		}
+
+	}
+}
