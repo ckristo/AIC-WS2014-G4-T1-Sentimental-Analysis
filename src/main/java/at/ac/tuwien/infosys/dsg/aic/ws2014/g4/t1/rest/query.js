861983b3880fb446d8e0413a30112a141feab510
Alexander Wurl <alex.wurl@gmail.com>
2015-01-02 16:50:59 +0100

    jquery, get-post

---
+++ src/main/java/at/ac/tuwien/infosys/dsg/aic/ws2014/g4/t1/rest/query.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$.ajax({
+        url: "http://localhost:9000/sentiment/test",
+        type: "POST"
+    }).then(function(data) {
+       
+       $.ajax({
+        url: "http://localhost:9000/sentiment/test/query",
+        type: "GET"
+    }).then(function(data) {
+       $('.greeting-id').append("test");
+       $('.greeting-content').append(data);
+    });
+    
+    });
+    
+    
+});
+
