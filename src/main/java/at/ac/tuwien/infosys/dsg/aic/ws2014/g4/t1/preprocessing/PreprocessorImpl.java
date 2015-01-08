8416a03f4999b3f73090037133136f912c1b87bc
v5r <github@b8000h.org>
2015-01-08 02:06:27 +0100

    remove repeated characters

--- src/main/java/at/ac/tuwien/infosys/dsg/aic/ws2014/g4/t1/preprocessing/PreprocessorImpl.java
+++ src/main/java/at/ac/tuwien/infosys/dsg/aic/ws2014/g4/t1/preprocessing/PreprocessorImpl.java
@@ -8,38 +8,43 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class PreprocessorImpl implements IPreprocessor {
-	
+
 	/**
 	 * RegEx pattern for a URL.
 	 * @see http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without
 	 */
 	private static final Pattern URL_PATTERN = Pattern.compile("^((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\\+\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?(?:[\\w]*))?)$");
-	
+
+	/**
+	 * Pattern for a character being repeated more than 3 times ("hellooooo")
+	 */
+	private static final Pattern REP_CHAR_PATTERN = Pattern.compile("(\\w)\\1{3,}");
+
 	/**
 	 * Dictionary used for stopword removal.
 	 */
 	private final StopwordsDictionary stopwordsDict = StopwordsDictionary.getInstance();
-	
+
 	/**
 	 * Dictionary used to expand abbreviations.
 	 */
 	private final AbbreviationsDictionary abbrevDict = AbbreviationsDictionary.getInstance();
-	
+
 	/**
 	 * Tokenizer used to tokenize an expanded abbreviation.
 	 */
 	private final ITokenizer tokenizer = new TokenizerImpl();
-	
+
 	/**
 	 * Dictionary used for spell checking & correction.
 	 */
 	private final SpellDictionary spellDict = SpellDictionary.getInstance();
-	
+
 	/**
 	 * The logger instance.
 	 */
 	private static final Logger logger = LogManager.getLogger(PreprocessorImpl.class);
-	
+
 	/**
 	 * Performs the preprocessing step.
 	 * @param tokens the list of raw tokens that will be manipulated by the preprocessor.
@@ -47,72 +52,75 @@ public class PreprocessorImpl implements IPreprocessor {
 	@Override
 	public void preprocess(List<String> tokens) {
 		logger.debug("* Preprocessing tweet:");
-		
+
 		ListIterator<String> iterator = tokens.listIterator();
 		String word, normalizedWord;
-		
+
 		while (iterator.hasNext()) {
 			word = iterator.next();
 			normalizedWord = normalize(word);
-			
+
 			logger.debug("  - preprocess normalized token '"+normalizedWord+"'");
-			
+
 			// (1) remove token if it's a stopword
 			if (stopwordsDict.containsWord(normalizedWord)) {
 				logger.debug("     --> stopword detected, remove it.");
-				
+
 				iterator.remove();
 				continue;
 			}
-			
+
 			// (2) replace URLs
 			if (isURL(normalizedWord)) {
 				logger.debug("     --> URL detected, replace it with URL-token.");
-				
+
 				iterator.set(URL_TOKEN);
 				continue;
 			}
-			
+
 			// (3) replace usernames
 			if (isUsername(normalizedWord)) {
 				logger.debug("     --> username detected, replace it with Username-token.");
-				
+
 				iterator.set(USERNAME_TOKEN);
 				continue;
 			}
-			
+
 			// (4) replace abbreviations and emoticons
 			if (abbrevDict.containsWord(normalizedWord)) {
 				String longForm = abbrevDict.getLongForm(normalizedWord);
-				
+
 				logger.debug("     --> abbreviation detected, replace it with tokens for '"+longForm+"'.");
-				
+
 				iterator.remove();
-				
+
 				// add tokens for the words in long form
 				for (String ad : tokenizer.tokenize(longForm)) {
 					iterator.add(ad);
 				}
 				continue;
 			}
-			
+
 			// (5) replace misspelled words
 			if (!spellDict.containsWord(normalizedWord)) {
 				String correction = spellDict.getSuggestion(normalizedWord);
 				if (correction != null) {
 					String replacement = normalize(correction);
 					logger.debug("     --> misspelled word detected, replace it with '"+replacement+"'");
-					
+
 					iterator.set(replacement);
 					continue;
 				}
 			}
-			
+
+			// (6) replace repeated characters (>3 repetitions)
+			normalizedWord = REP_CHAR_PATTERN.matcher(normalizedWord).replaceAll("$1");
+
 			// normalize the token
 			iterator.set(normalizedWord);
 		}
 	}
-	
+
 	/**
 	 * Checks if a given string is an URL.
 	 * @param str the string to check
@@ -122,7 +130,7 @@ public class PreprocessorImpl implements IPreprocessor {
 		Matcher m = URL_PATTERN.matcher(str);
 		return m.matches();
 	}
-	
+
 	/**
 	 * Checks if a given string is a mentioned Twitter username (starting with @).
 	 * @param str the string to check
@@ -131,7 +139,7 @@ public class PreprocessorImpl implements IPreprocessor {
 	private boolean isUsername(String str) {
 		return str.startsWith("@");
 	}
-	
+
 	/**
 	 * Performs normalization of a token.
 	 * @param str the string to normalize
--- src/test/java/at/ac/tuwien/infosys/dsg/aic/ws2014/g4/t1/PreprocessorTest.java
+++ src/test/java/at/ac/tuwien/infosys/dsg/aic/ws2014/g4/t1/PreprocessorTest.java
@@ -16,91 +16,105 @@ import static org.junit.Assert.*;
 public class PreprocessorTest {
 
 	IPreprocessor preprocessor;
-	
+
 	@Before
 	public void setUp() {
 		preprocessor = new PreprocessorImpl();
 	}
-	
+
 	public void tearDown() {
 		preprocessor = null;
 	}
-	
+
 	@Test
 	public void testRemoveStopwords() {
 		List<String> tokens = new ArrayList<>();
 		tokens.add("a");
 		tokens.add("the");
 		tokens.add("test");
-		
+
 		preprocessor.preprocess(tokens);
-		
+
 		assertFalse(tokens.contains("a"));
 		assertFalse(tokens.contains("the"));
 		assertTrue(tokens.contains("test"));
 	}
-	
+
 	@Test
 	public void testReplaceURLs_1() {
 		String testUrl = "http://www.example.com/test/index.php?arg1=val1&arg2=val2";
 		List<String> tokens = new ArrayList<>();
 		tokens.add(testUrl);
-		
+
 		preprocessor.preprocess(tokens);
-		
+
 		assertFalse(tokens.contains(testUrl));
 		assertTrue(tokens.contains(IPreprocessor.URL_TOKEN));
 	}
-	
+
 	@Test
 	public void testReplaceURLs_2() {
 		String testUrl = "mailto:test@example.com";
 		List<String> tokens = new ArrayList<>();
 		tokens.add(testUrl);
-		
+
 		preprocessor.preprocess(tokens);
-		
+
 		assertFalse(tokens.contains(testUrl));
 		assertTrue(tokens.contains(IPreprocessor.URL_TOKEN));
 	}
-	
+
 	@Test
 	public void testReplaceUsernames() {
 		List<String> tokens = new ArrayList<>();
 		tokens.add("@test");
-		
+
 		preprocessor.preprocess(tokens);
-		
+
 		assertFalse(tokens.contains("@test"));
 		assertTrue(tokens.contains(IPreprocessor.USERNAME_TOKEN));
 	}
-	
+
 	@Test
 	public void testReplaceAbbreviations_1() {
 		List<String> tokens = new ArrayList<>();
 		tokens.add("lol");
 		tokens.add("wth");
-		
+
 		preprocessor.preprocess(tokens);
-		
+
 		assertTrue(tokens.contains("laughing"));
 		assertTrue(tokens.contains("out"));
 		assertTrue(tokens.contains("loud"));
 		assertTrue(tokens.contains("what"));
 		assertTrue(tokens.contains("hell"));
 	}
-	
+
 	@Test
 	public void testReplaceMisspelledWords() {
 		List<String> tokens = new ArrayList<>();
 		tokens.add("huose");
 		tokens.add("ball");
-		
+
 		preprocessor.preprocess(tokens);
-		
+
 		assertFalse(tokens.contains("huose"));
 		assertTrue(tokens.contains("house"));
 		assertTrue(tokens.contains("ball"));
 	}
-	
+
+	@Test
+	public void testRemoveRepeatedChars() {
+		List<String> tokens = new ArrayList<>();
+		tokens.add("helloooooo");
+		tokens.add("teeeestinnnnng");
+		tokens.add("trees");
+
+		preprocessor.preprocess(tokens);
+
+		assertTrue(tokens.contains("hello"));
+		assertTrue(tokens.contains("testing"));
+		assertTrue(tokens.contains("trees"));
+	}
+
 }
