9176431de2e03166a63c7e366f2757e3668953dc
v5r <github@b8000h.org>
2015-01-12 18:03:38 +0100

    longer timeout for AJAX request
    (downloading and analyzing tweets can take a long time)

--- src/main/resources/web/query.js
+++ src/main/resources/web/query.js
@@ -6,7 +6,9 @@ $(document).ready(function() {
 
        $.ajax({
         url: "http://localhost:9000/sentiment/test/query",
-        type: "GET"
+        type: "GET",
+        timeout: 30000   // timeout 30 seconds (query might take some time)
+                         //TODO: display progress indicator (spinner etc.)
     }).then(function(data) {
        $('.greeting-id').append("test");
        $('.greeting-content').append(data);
